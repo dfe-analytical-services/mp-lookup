1aa7b1eb85feb5a78c6ed8efaee9e8a47e105570
Rich Bielby <richbielby@e.email>
2025-07-30 15:08:29 +0100

    Bugfix/extract_email not found (#4)
    * Switched extract_emails into separate script
    
    * Added conditional to stop workflow completing when run from a PR

--- data_update.R
+++ data_update.R
@@ -4,6 +4,8 @@ library(dplyr)
 library(tidyr)
 library(testthat)
 
+source("R/utils.R")
+
 # Create lookup ===============================================================
 mp_lookup <- dfeR::fetch_pcons(2024, "England") |>
   dplyr::mutate(pcon_name_lower = tolower(pcon_name)) |>
@@ -81,34 +83,3 @@ write.csv(
   file = "mp_lookup.csv",
   row.names = FALSE
 )
-
-# Function to extract member id and member email address from the mnis base
-# output
-# To be used with mnis base output list and apply(..., 2, extract_email)
-extract_email <- function(mnis_base_output_line) {
-  member_id <- mnis_base_output_line |>
-    magrittr::extract2("@Member_Id")
-  member_emails <- mnis_base_output_line |>
-    magrittr::extract2("Addresses.Address") |>
-    magrittr::extract2("Email")
-  # Remove NAs, trim spaces, switch to lower case and remove duplicates
-  member_emails <- member_emails[!is.na(member_emails)] |>
-    stringr::str_trim() |>
-    tolower() |>
-    unique()
-  # Choose a preferred email in the case of multiple and "No email found" in the
-  # case of a NULL result
-  if (any(grepl("mp@parliament.uk", member_emails))) {
-    member_email <- member_emails[grepl("mp@parliament.uk", member_emails)][1]
-  } else if (any(grepl("parliament.uk", member_emails))) {
-    member_email <- member_emails[grepl("parliament.uk", member_emails)][1]
-  } else if (length(member_emails) > 1) {
-    member_email <- member_emails[1]
-  } else if (length(member_emails) == 0) {
-    member_email <- "No email found"
-    warning("No email found for member_id: ", member_id)
-  } else {
-    member_email <- member_emails
-  }
-  return(list(member_id = member_id, member_email = member_email))
-}
