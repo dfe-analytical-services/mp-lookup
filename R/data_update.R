fb336e7ec865bf8976c5c6abe9b11decc2d79960
Cam Race <52536248+cjrace@users.noreply.github.com>
2025-09-30 10:26:10 +0100

    tidy up handling of legacy codes

--- R/data_update.R
+++ R/data_update.R
@@ -78,6 +78,7 @@ mp_lookup <- mp_lookup |>
 
 # Add on LADs =================================================================
 lad_summary <- dfeR::geo_hierarchy |>
+  dplyr::filter(most_recent_year_included == 2025) |>
   dplyr::group_by(pcon_code) |>
   dplyr::arrange(lad_code) |>
   dplyr::summarise(
@@ -85,25 +86,48 @@ lad_summary <- dfeR::geo_hierarchy |>
     lad_codes = paste(unique(lad_code), collapse = " / ")
   )
 
+legacy24_lad_summary <- dfeR::fetch_lads(2024) |>
+  dplyr::left_join(
+    dfeR::geo_hierarchy |>
+      dplyr::filter(most_recent_year_included > 2024) |>
+      dplyr::select(pcon_code, lad_name),
+    by = "lad_name"
+  ) |>
+  dplyr::group_by(pcon_code) |>
+  dplyr::summarise(
+    lad_codes_2024 = paste(unique(lad_code), collapse = " / ")
+  )
+
 mp_lookup <- mp_lookup |>
-  dplyr::left_join(lad_summary, by = "pcon_code")
+  dplyr::left_join(lad_summary, by = "pcon_code") |>
+  dplyr::left_join(legacy24_lad_summary, by = "pcon_code")
 
 # Add on LAs ==================================================================
 la_summary <- dfeR::geo_hierarchy |>
+  dplyr::filter(most_recent_year_included == 2025) |>
   dplyr::group_by(pcon_code) |>
   dplyr::arrange(new_la_code) |>
   dplyr::summarise(
     la_names = paste(unique(la_name), collapse = " / "),
-    old_la_codes = paste(unique(old_la_code), collapse = " / "),
     new_la_codes = paste(unique(new_la_code), collapse = " / ")
+  )
+
+legacy24_la_summary <- dfeR::fetch_las(2024) |>
+  dplyr::left_join(
+    dfeR::geo_hierarchy |>
+      dplyr::filter(most_recent_year_included > 2024) |>
+      dplyr::distinct(pcon_code, la_name),
+    by = "la_name"
   ) |>
-  # strip out " / z" from old_la_codes (as GIAS doesn't yet have 2025)
-  dplyr::mutate(
-    old_la_codes = gsub(" / z$", "", old_la_codes)
+  dplyr::group_by(pcon_code) |>
+  dplyr::summarise(
+    new_la_codes_2024 = paste(unique(new_la_code), collapse = " / "),
+    old_la_codes_2024 = paste(unique(old_la_code), collapse = " / ")
   )
 
 mp_lookup <- mp_lookup |>
-  dplyr::left_join(la_summary, by = "pcon_code")
+  dplyr::left_join(la_summary, by = "pcon_code") |>
+  dplyr::left_join(legacy24_la_summary, by = "pcon_code")
 
 # Add on Mayoral Authorities ==================================================
 mayoral_summary <- dfeR::geo_hierarchy |>
@@ -112,8 +136,14 @@ mayoral_summary <- dfeR::geo_hierarchy |>
   dplyr::filter(english_devolved_area_name != "Not applicable") |> # add back in later to avoid
   # ...unnecessary "Not applicable" entries in the concatenated strings
   dplyr::summarise(
-    mayoral_auth_names = paste(unique(english_devolved_area_name), collapse = " / "),
-    mayoral_auth_codes = paste(unique(english_devolved_area_code), collapse = " / ")
+    mayoral_auth_names = paste(
+      unique(english_devolved_area_name),
+      collapse = " / "
+    ),
+    mayoral_auth_codes = paste(
+      unique(english_devolved_area_code),
+      collapse = " / "
+    )
   )
 
 mp_lookup <- mp_lookup |>
@@ -146,9 +176,11 @@ expected_cols <- c(
   "election_result_summary_2024",
   "lad_names",
   "lad_codes",
+  "lad_codes_2024",
   "la_names",
-  "old_la_codes",
   "new_la_codes",
+  "new_la_codes_2024",
+  "old_la_codes_2024",
   "mayoral_auth_names",
   "mayoral_auth_codes"
 )
@@ -200,12 +232,18 @@ test_that("All codes follow expected pattern", {
   expect_true(all(grepl("^[A-Za-z0-9]{9}$", mp_lookup$pcon_code)))
 
   main_code_pattern <- "^([A-Za-z0-9]{9}|z)( / ([A-Za-z0-9]{9}|z))*$"
-  for (col in c("lad_codes", "new_la_codes", "mayoral_auth_codes")) {
+  for (col in c(
+    "lad_codes",
+    "lad_codes_2024",
+    "new_la_codes",
+    "new_la_codes_2024",
+    "mayoral_auth_codes"
+  )) {
     expect_true(all(grepl(main_code_pattern, mp_lookup[[col]])))
   }
 
   three_digit_pattern <- "^([0-9]{3}|z)( / ([0-9]{3}|z))*$"
-  expect_true(all(grepl(three_digit_pattern, mp_lookup$old_la_codes)))
+  expect_true(all(grepl(three_digit_pattern, mp_lookup$old_la_codes_2024)))
 })
 
 test_that("All constituency names are within the dfeR pcons", {
